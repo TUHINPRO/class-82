4c19c1286d484e5b60ebba9df730d10ae77eaf26
TUHINPRO <71925866+TUHINPRO@users.noreply.github.com>
2020-11-19 15:44:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+var mouseEvent="empty";
+var last_position_of_x,last_position_of_y;
+
+var canvas=document.getElementById("myCanvas");
+var ctx=canvas.getContext("2d");
+
+color="black";
+width=1;
+canvas.addEventListener("mousedown",my_mousedown);
+
+function my_mousedown (e) {
+    mouseEvent="mousedown";
+
+    color=document.getElementById("color").value;
+    width=document.getElementById("widthofline").value;
+}
+
+canvas.addEventListener("mouseup",my_mouseup);
+
+function my_mouseup(e) {
+    mouseEvent="mouseup";
+}
+
+canvas.addEventListener("mouseleave",my_mouseleave);
+
+function my_mouseleave(e) {
+    mouseEvent="mouseleave";
+    }
+
+    canvas.addEventListener("mousemove",my_mousemove);
+
+    function my_mousemove(e) {
+        current_position_of_mouse_x=e.clientX-canvas.offsetLeft;
+        current_position_of_mouse_y=e.clientY-canvas.offsetTop;
+
+        if(mouseEvent="mousedown") {
+            ctx.beginPath();
+            ctx.strokeStyle=color;
+            ctx.lineWidth=width;
+
+            ctx.moveTo(last_position_of_x,last_position_of_y);
+            ctx.lineTo(current_position_of_mouse_x,current_position_of_mouse_y);
+
+            ctx.stroke();
+            }
+            last_position_of_x=current_position_of_mouse_x;
+            last_position_of_y=current_position_of_mouse_y;
+
+    }
+    function clearArea() {
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+    }
+
